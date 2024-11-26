305d96c6a1e7461865965490278dec38bbf44e72
Nalladurai <nalladurai.subash@gove.co>
2024-11-26 16:26:57 +0530

    Update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = class Configuration {
-    GATEWAY_URL = 'http://localhost:3000/api/rest/tms/dataaccess/1.0.0/';
+    GATEWAY_URL = 'https://tms-dataaccesslayer.onrender.com/';
     GATEWAY_USERNAME = 'QP0192923232LD';
     GATEWAY_PASSWORD = '927NBGJJ0283HKA74782';
     HEADERS = {
